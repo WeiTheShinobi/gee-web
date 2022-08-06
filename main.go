89b4dd3cf7b9fb60554cb1a0d7103ca2e5048065
WeiTheShinobi <weitheshinobi@gmail.com>
2022-08-07 00:22:20 +0800

    routerGroup

--- main.go
+++ main.go
@@ -12,19 +12,16 @@ func main() {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.String(http.StatusOK, "gugugu")
+		})
 
-	r.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "gu!")
+		})
+	}
 
 	r.Run(":8080")
 }
